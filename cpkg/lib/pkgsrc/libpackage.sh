8c0bae18bf343c3e7318bccb845ed88c91e3cda5
Remy Chibois <chybz@chybz.net>
2014-08-24 20:05:25 +0200

    external usability fixes

--- cpkg/lib/pkgsrc/libpackage.sh
+++ cpkg/lib/pkgsrc/libpackage.sh
@@ -36,7 +36,7 @@ function lp_handle_package_files() {
 
     if [[ -d $PKG_STAGEDIR/$PKG_SYSETCDIR ]]; then
         mkdir -p $PKG_STAGEDIR/$PKG_SHAREDIR
-        [[ $PKG_STAGEDIR/$PKG_SHAREDIR/etc ]] && \
+        [[ -d $PKG_STAGEDIR/$PKG_SHAREDIR/etc ]] && \
             rm -rf $PKG_STAGEDIR/$PKG_SHAREDIR/etc
         mv $PKG_STAGEDIR/$PKG_SYSETCDIR $PKG_STAGEDIR/$PKG_SHAREDIR/etc
     fi
