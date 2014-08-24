22b2411165effd2da97175748596e31acc6a2eea
Remy Chibois <chybz@chybz.net>
2014-08-24 18:31:43 +0200

    pkgsrc workflow fixes

--- cpkg/lib/pkgsrc/libpackage.sh
+++ cpkg/lib/pkgsrc/libpackage.sh
@@ -71,7 +71,7 @@ function lp_build_package() {
     sudo bmake stage-install CHECK_FILES=no
     sudo bmake print-PLIST > PLIST
     pkglint
-    sudo bmake clean
+    sudo bmake stage-install
     sudo bmake package
 }
 
