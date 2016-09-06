e3805dde315eeebe93888d86e03296f5b11a9fd8
Remy Chibois <chybz@chybz.net>
2016-09-06 10:17:17 +0200

    fixing pkglint pb

--- cpkg/lib/libcpkg.sh
+++ cpkg/lib/libcpkg.sh
@@ -529,6 +529,7 @@ function cp_set_git_variables() {
             export PKG_FROM_GITHUB=1
             PKG_GIT_SUBDIR=1
             PKG_GIT_REPO=${PKG_GIT_REPO%\.git}
+            export PKG_GITHUB_USER=${PKG_GIT_REPO%/*}
             PKG_GIT_URL="https://$PKG_GIT_HOST/$PKG_GIT_REPO"
             PKG_GIT_FETCH_URL="$PKG_GIT_URL/archive/"
         else
