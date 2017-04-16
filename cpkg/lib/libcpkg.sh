47b047b90ab4cbc9cb97d6c535dc70fe5025948c
Rémy Chibois <chybz@chybz.net>
2017-04-16 12:00:19 +0200

    fixing github host

--- cpkg/lib/libcpkg.sh
+++ cpkg/lib/libcpkg.sh
@@ -527,6 +527,7 @@ function cp_set_git_variables() {
 
         if [[ $URL =~ github\.com ]]; then
             export PKG_FROM_GITHUB=1
+            PKG_GIT_HOST="github.com"
             PKG_GIT_SUBDIR=1
             PKG_GIT_REPO=${PKG_GIT_REPO%\.git}
             export PKG_GITHUB_USER=${PKG_GIT_REPO%/*}
